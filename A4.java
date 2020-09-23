0e8ad0b387be9e09803376bd9a0b211a7255ef51
Chinmay Mungilwar <chinmaym49@gmail.com>
2020-09-23 20:22:54 +0530

    get, set name, id, priority

--- A4.java
+++ A4.java
@@ -3,16 +3,28 @@ public class A4 extends Thread {
     public void run() {
         System.out.println(Thread.currentThread().getId());
         System.out.println(Thread.currentThread().getName());
+        System.out.println(Thread.currentThread().getPriority());
         System.out.println("Thread running!");
     }
 
     public static void main(String[] args) {
-        A4 t=new A4();
-        System.out.println(t.getId());
-        System.out.println(t.getName());
-        t.setName("th1");
-        System.out.println(t.getName());
-        t.start();
+        A4 t1=new A4();
+        System.out.println(t1.getId());
+        System.out.println(t1.getName());
+        t1.setName("th1");
+        System.out.println(t1.getName());
+        
+        A4 t2=new A4();
+        System.out.println(t2.getId());
+        System.out.println(t2.getName());
+        t2.setName("th2");
+        System.out.println(t2.getName());
+
+        t1.setPriority(7);
+        t2.setPriority(1);
+        t2.start();
+        t1.start();
+
     }
 
 }
@@ -20,6 +32,14 @@ public class A4 extends Thread {
 // 13
 // Thread-0
 // th1
+// 14
+// Thread-1
+// th2
+// 14
 // 13
 // th1
+// th2
+// 1
+// 7
+// Thread running!
 // Thread running!
